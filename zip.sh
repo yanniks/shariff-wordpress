8a2b4b759661f4e220ec83a9eae13f785352a1e9
yanniks <kontakt@yanniks.de>
2014-11-30 19:24:35 +0100

    workaround

--- zip.sh
+++ zip.sh
@@ -6,5 +6,7 @@ mkdir -p shariff-wp/dep
 cp shariff/build/shariff.min.* shariff-wp/dep
 cp -R backend-php/build/src shariff-wp/backend/src
 cp -R backend-php/build/vendor shariff-wp/backend/vendor
+# remove backend/vendor/guzzlehttp/guzzle/tests/perf.php so WordPress.org doesn't complain
+rm -f shariff-wp/backend/vendor/guzzlehttp/guzzle/tests/perf.php
 zip -r shariff-wp-$version.zip shariff-wp
 rm -rf shariff-wp
